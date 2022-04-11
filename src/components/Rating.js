f2d2a4caea8c20897d3466e28c920071fd2ac7cd
Madeleine Woodbury <randine25@gmail.com>
2022-04-11 12:47:53 +0200

    form

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,7 +3,7 @@ import starIcon from "../images/icon-star.svg";
 const Rating = () => {
   return (
     <div className='rating'>
-      <div className='star'>
+      <div className='circle'>
         <img src={starIcon} alt='star icon' />
       </div>
       <div className='content'>
@@ -13,6 +13,26 @@ const Rating = () => {
           is appreciated to help us improve our offering!
         </p>
       </div>
+      <form>
+        <div className='form-container'>
+          <div className='input circle'>
+            <span>1</span>
+          </div>
+          <div className='input circle'>
+            <span>2</span>
+          </div>
+          <div className='input circle'>
+            <span>3</span>
+          </div>
+          <div className='input circle'>
+            <span>4</span>
+          </div>
+          <div className='input circle'>
+            <span>5</span>
+          </div>
+        </div>
+        <button className='btn'>SUBMIT</button>
+      </form>
     </div>
   );
 };
