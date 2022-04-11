d0638f6f47863522d7e259f3de8fdec5c7520fc8
Madeleine Woodbury <randine25@gmail.com>
2022-04-11 12:30:01 +0200

    Content, title and paragraph

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -6,6 +6,13 @@ const Rating = () => {
       <div className='star'>
         <img src={starIcon} alt='star icon' />
       </div>
+      <div className='content'>
+        <h2 className='title'>How did we do?</h2>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+      </div>
     </div>
   );
 };
